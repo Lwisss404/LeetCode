8c8e818c6c0baf77897a4a8647d6653511c7a396
Lwisss404 <wassimbnslamaios@gmail.com>
2025-09-25 19:48:39 +0200

    My First Attempt

--- 1-Two Sums.c
+++ 1-Two Sums.c
@@ -1,15 +1,23 @@
 #include <stdlib.h>
 
+void fillArray(int *nums, int numsSize) {
+    for (int i = 0; i < numsSize; i++) {
+        *(nums + i) = 1 + (rand() % 100);
+    }
+}
+
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     *returnSize = 2;  // immer 2 Werte zurück
     int* resArray = malloc(2 * sizeof(int));
     if (!resArray) return NULL; // malloc-Fehler abfangen
 
+    printf("Debugg: \n");
     for (int i = 0; i < numsSize; i++) {
         for (int j = i + 1; j < numsSize; j++) {
-            if (nums[i] + nums[j] == target) {
-                resArray[0] = i; // Index zurückgeben (wie in LeetCode erwartet)
-                resArray[1] = j;
+            printf("%d[Element n° %d] + %d[Element n° %d] = %d", nums[i], i+1, nums[j], j+1, nums[i] + nums[j]);
+            if (*(nums + i) + *(nums + j) == target) {
+                *(resArray) = *(nums + i);
+                *(resArray + 1) = *(nums + j);
                 return resArray;
             }
         }
@@ -17,6 +25,19 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
 
     // Falls nichts gefunden wurde
     *returnSize = 0;
-    free(resArray);
     return NULL;
+}
+
+int main() {
+    int numsSize = 5 + (rand() % (20 - 5 + 1));
+    int* nums = malloc(numsSize * sizeof(int));
+    int* returnSize = malloc(sizeof(int));
+    fillArray(nums, numsSize);
+    int* resArray = twoSum(nums, numsSize, 9, returnSize);
+    if (!resArray) return 0;
+    printf("[%d, %d]", resArray[0], resArray[1]);
+    free(nums);
+    free(returnSize);
+    free(resArray);
+    return 0;
 }
