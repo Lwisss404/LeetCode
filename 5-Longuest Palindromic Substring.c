884a520b31b6d23a0afed501a2825e2389476ee1
Lwisss404 <wassimbnslamaios@gmail.com>
2025-09-27 20:08:41 +0200

    #

--- 5-Longuest Palindromic Substring.c
+++ 5-Longuest Palindromic Substring.c
@@ -1,7 +1,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+//
 int isPalindrome(char* s, int start, int end) {
     while (start < end) {
         if (s[start] != s[end]) {
@@ -46,3 +46,4 @@ int main() {
     free(longPal);
     return 0;
 }
+
