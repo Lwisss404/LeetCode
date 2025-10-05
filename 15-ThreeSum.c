f863beee846fe46d05a08cf12b6bf2d567134700
Lwisss404 <wassimbnslamaios@gmail.com>
2025-10-05 20:58:14 +0200

    ##

--- 15-ThreeSum.c
+++ 15-ThreeSum.c
@@ -17,16 +17,6 @@ void displayArray(int* nums, int numsSize) {
     printf("]\n");
 }
 
-int binarySearch(int* nums, int start, int end, int target) {
-    while (start <= end) {
-        int mid = start + (end - start) / 2;
-        if (nums[mid] == target) return 1;
-        else if (nums[mid] > target) end = mid - 1;
-        else start = mid + 1;
-    }
-    return 0;
-}
-
 int cmp(const void* a, const void* b) {
     return *(int *)a - *(int *)b;
 }
@@ -37,7 +27,7 @@ int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSize)
 
     qsort(nums, numsSize, sizeof(int), cmp);
 
-    int capacity = 1000;
+    int capacity = 64;
     int** resultArray = malloc(capacity * sizeof(int *));
     *returnColumnSize = malloc(capacity * sizeof(int));
     if (resultArray == NULL || (*returnColumnSize) == NULL) return NULL;
@@ -45,32 +35,41 @@ int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSize)
     for (int i = 0; i < numsSize - 2; i++) {
         //skips duplicates
         if (i > 0 && *(nums + i) == *(nums + i - 1)) continue;
+        //break is all numbers are positiv or negative
+        if (nums[i] > 0 || nums[numsSize - 1] < 0) break;
+        
+        int target = -nums[i];
+        int left = i + 1;
+        int right = numsSize - 1;
 
-        for (int j = i + 1; j < numsSize - 1; j++) {
-            //also skips duplicates
-            if (j > i + 1 && *(nums + j) == *(nums + j - 1)) continue;
-            int complement = -nums[i] - nums[j];
-            int foundComp = binarySearch(nums, j + 1, numsSize - 1, complement);
-
-            int* triplet = malloc(3 * sizeof(int));
-            if (foundComp) {
+        while (left < right) {
+            int complement = nums[left] + nums[right];
+            if(complement == target) {
                 //complement found
+                int* triplet = malloc(3 * sizeof(int));
                 triplet[0] = nums[i];
-                triplet[1] = nums[j];
-                triplet[2] = complement;
-                qsort(triplet, 3, sizeof(int), cmp);
-
+                triplet[1] = nums[left];
+                triplet[2] = nums[right];
+                
                 //dynamic space reallocation in case of overflow
                 if (*returnSize >= capacity) {
-                    capacity *= 1.5;
+                    capacity *= 2;
                     resultArray = realloc(resultArray, capacity * sizeof(int *));
                     *returnColumnSize = realloc(*returnColumnSize, capacity * sizeof(int));
                 }
                 resultArray[*returnSize] = triplet;
                 (*returnColumnSize)[*returnSize] = 3;
                 (*returnSize)++;
+                left++;
+                right--;
+
+                //skipping dupes
+                while (left < right && nums[left] == nums[left - 1]) left++;
+                while (left < right && nums[right] == nums[right + 1]) right--;
+            } else if (complement < target) {
+                left++;
             } else {
-                free(triplet);
+                right--;
             }
         }
     }
